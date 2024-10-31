2201efab6e63e1a25f9451ef70c0bfbbfb30d165
Alan Carrizo <alancarrizo1017@gmail.com>
2024-10-30 21:17:38 -0300

    Estructura inicial WEB/API para proyecto Minimarket

---
+++ WEB/src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import '../styles/App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1>Minimarket Bitward</h1>
+      <button onClick={() => setCount(count + 1)}>Incrementar</button>
+      <button onClick={() => setCount(count - 1)}>Decrementar</button>
+      <p>{count}</p>
+    </>
+  )
+}
+
+export default App
