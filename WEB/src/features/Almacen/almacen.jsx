21c4a8b1737eb68317e9a5d3a8bf66a2bf181fc1
Francisco <francisco123@gmail.com>
2024-11-23 21:57:12 -0300

    cambios en WEB:Almacen

--- WEB/src/features/Almacen/almacen.jsx
+++ WEB/src/features/Almacen/almacen.jsx
@@ -1,92 +1,168 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import './styles/almacen.css'
-// import useAlmacen from '../../hooks/Almacen/useAlmacen.jsx';
+import useAlmacen from '../../hooks/Almacen/useAlmacen.jsx';
 
 function Almacen() {
-  
+  const {getAlmacen, postAlmacen, putAlmacen, deleteAlmacen, setError,error, loading} =useAlmacen()
+  const [nombre, setNombre] = useState(" ")
+  const [descripcion, setDescripcion] = useState("")
+  const [categoria, setCategoria] = useState("")
+  const [proveedor, setProveedor] = useState("")
+  const [precio, setPrecio] = useState("")
+  const [cantidad, setCantidad] = useState("")
+  const [imagen, setImagen] = useState("")
+  const [productoId, setProductoId] = useState(0)
+  const[listaProductos, setListaProductos] = useState([])
+  const [onEdit, setOnEdit] = useState(false)
+  const [reload, setReload] = useState(false) 
+    
+    const mostrarProductos = async ()=>{
+      try {
+        const response = await getAlmacen();
+        console.log('Data recibida:', response);
+        
+        setListaProductos(response.data.productosConDetalles);
+      } catch (error) {
+        console.error(error);
+      }
+
+    }
+
+    
+    useEffect(() => {
+      mostrarProductos();
+    }, [reload]);
      
-    const handleSubmit = async (e) => {
-        e.preventDefault();
+    const handleSubmit = async (e) =>{
+      e.preventDefault()
+      
+      const nuevoProducto = {
+        id: productoId,
+        nombre: nombre,
+        descripcion: descripcion,
+        categoria: categoria,
+        proveedor: proveedor,
+        precio: precio,
+        cantidad: cantidad,
+        imagen: imagen
+      }
+      const result = await postAlmacen(nuevoProducto)
+      if(result.success){
+          setReload(!reload)
+          console.log("Errores:", error)
+          console.log("Proveedor creado con éxito.", result.data)
+          setError(null)
+      } else{
+          console.error("Error al crear el proveedor.", result.message)
+      }
+      
+      
+    }
 
-        if(productoId===0){
-            useProductos()
+    const actualizarProducto = async()=>{
+      const nuevoProducto = {
+          id: proveedorID,
+          nombre: nombre,
+          descripcion: descripcion,
+      }
+      const result = await putAlmacen(nuevoProducto)
+        if(result.success){
+          setReload(!reload)
+          console.log("Productos actualizado con éxito.", result.data)
+          setNombre('')
+          setDescripcion('')
+          setOnEdit(false)
+          setProveedorID(null)
+          setError(null)
+        } else{
+          console.error("Error al actualizar los Productos.", result.message)
+          setNombre('')
+          setDescripcion('')
+          setOnEdit(false)
+          setProveedorID(null)
         }
+    }
 
-        //limpiar formulario
-        setNombre("");
-        setDescripcion("");
-        setCategoria("");
-        setProveedor("");
-        setPrecio(0);
-        setCantidad(0);
-        setImagen("");
-        setProductoId(0);
-    };
+    
     
     const eliminarProducto = async (id) => {
-        if (confirm("¿Desea quitar el producto?")) {
-          const response = await fetch(`http://localhost:3000/administrador/${id}`, {
-            method: "DELETE",
-          });
-    
+        if (confirm(`¿Desea quitar el productO ${id}?`)) {
+          const result = await deleteAlmacen(id)
+          if (result.succes){
+            console.log("producto eliminado")
+            setReload(!reload)
+          }else{
+            console.error("eror al eliminar producto",result.message)
+          }
+        }
           if (response.ok) {
             setProducto(productos.filter((producto) => producto.id !== id));
           }
-        }
-    };
-
-    const modificarProducto = (producto) => {
-        setProductoId(producto.id);
-        setNombre(producto.nombre)
-        setDescripcion(producto.descripcion)
-        setCategoria(producto.categoria)
-        setProveedor(producto.proveedor)
-        setPrecio(producto.precio)
-        setCantidad(producto.cantidad)
-        setImagen(producto.imagen)
-    };
+      }
+        useEffect(() => {
+          mostrarProductos();
+        }, [reload]); 
+      
+        useEffect(() => {
+          console.log("Lista actualizada: ", listaProductos); 
+        }, [listaProductos]);
     
+      return(
+        <div className='almacen-container'>
+          <h1 className='titulo'>Listado del almacen</h1><br />
+          <div className='contenedor-listado'>
+            {listaProductos && listaProductos.length > 0 ? (
+                listaProductos.map(producto => (
+                    <div key={producto.producto_id}>
+                        <img src={producto.imagen_url} alt={producto.nombre} />
+                        <p>{`${producto.producto_id} - ${producto.producto_nombre} - ${producto.descripcion} - P: ${producto.categoria_nombre} - ${producto.cantidad_disponible}}U - ${producto.precio}$`}</p>
+                        <button className="modificar" onClick={() => modificarProducto(producto)}>Modificar</button>
+                        <button className="eliminar" onClick={() => eliminarProducto(producto.id)}>Eliminar</button>
+                    </div>
+                ))
+            ) : (
+                <p>No hay productos disponibles.</p>
+            )}
+        </div>
+          <div className='contenedor-entradas'>
+                <form onSubmit={handleSubmit}>
+                    <label >Nombre:</label>
+                    <input  id="nombre" type="text"  value={nombre}onChange={(e) => setNombre(parseFloat(e.target.value))}/> 
+                    <label>Descripcion:</label> 
+                    <input id="descripcion" type="text" value={descripcion}
+                    onChange={(e) => setDescripcion(parseFloat(e.target.value))}/> 
+                    <label >Categoria:</label>
+                    <input id="apellido" type="text" value={categoria}
+                    onChange={(e) => setCategoria(parseFloat(e.target.value))}/> 
+                    <label >Proveedor:</label>
+                    <input id="proveedor" type="text"value={proveedor}
+                    onChange={(e) => setProveedor(parseFloat(e.target.value))} />
+                    <label >Precio:</label><input   id="precio" type="number" value={precio}
+                    onChange={(e) => setPrecio(parseFloat(e.target.value))} />
+                    <label >Cantidad:</label><input maxLength="2"type="number" name="" id="cantidad" value={cantidad}
+                    onChange={(e) => setCantidad(parseFloat(e.target.value))}/>
+                    <label >Imagen URL:</label><input maxLength={255} id="imagen" type="url" value={imagen}
+                    onChange={(e) => setImagen(parseFloat(e.target.value))} />
+                    <div className="contenedor-botones">
+                        <button onClick={()=> {modificarProducto(producto)}}>Guardar</button>
+                        <button onClick={() => {
+                          setProductoId(null)
+                          setNombre(""); 
+                          setDescripcion("");
+                          setCategoria("");
+                          setProveedor("");
+                          setPrecio("");
+                          setCantidad("");
+                          setImagen("")
+                          setOnEdit(false)}}>Cancelar</button>
+                        
+                    </div>
+                </form>
+          </div>
+        </div>
+      )
+};
 
-  return (
-    <div className='almacen-container'>
-      <h1 className='titulo'>Listado del almacen</h1><br />
-      <div className='contenedor-listado'>
-          {productos.map(producto => (
-              <div key={producto.id}>
-                  <img src={producto.imagen} alt={producto.nombre} />
-                  <p>{`${producto.id} - ${producto.nombre} - ${producto.categoria} - P: ${producto.proveedor} - ${producto.descripcion} - ${producto.cantidad}U - ${producto.precio}$`}</p>
-                  <button className="modificar" onClick={() => modificarProducto(producto)}>Modificar</button>
-                  <button className="eliminar" onClick={() => eliminarProducto(producto.id)}>Eliminar</button>
-              </div>
-          ))}
-      </div>
-      <div className='contenedor-entradas'>
-            <form onSubmit={handleSubmit}>
-                <label >Nombre:</label>
-                <input maxLength={10}  id="nombre" type="text"  value={nombre}onChange={(e) => setNombre(parseFloat(e.target.value))}/> 
-                <label>Descripcion:</label> 
-                <input id="descripcion" type="text" value={descripcion}
-                onChange={(e) => setDescripcion(parseFloat(e.target.value))}/> 
-                <label >Categoria:</label>
-                <input id="apellido" type="text" value={categoria}
-                onChange={(e) => setCategoria(parseFloat(e.target.value))}/> 
-                <label >Proveedor:</label>
-                <input id="proveedor" type="text"value={proveedor}
-                onChange={(e) => setProveedor(parseFloat(e.target.value))} />
-                <label >Precio:</label><input   id="precio" type="number" value={precio}
-                onChange={(e) => setPrecio(parseFloat(e.target.value))} />
-                <label >Cantidad:</label><input maxLength="2"type="number" name="" id="cantidad" value={cantidad}
-                onChange={(e) => setCantidad(parseFloat(e.target.value))}/>
-                <label >Imagen URL:</label><input maxLength={255} id="imagen" type="url" value={imagen}
-                onChange={(e) => setImagen(parseFloat(e.target.value))} />
-                <div class="contenedor-botones">
-                    <button>Guardar</button>
-                    <button onClick={() => {setNombre(""); setDescripcion("");setCategoria("");setProveedor("");setPrecio("");setCantidad("");setImagen("")}}>Cancelar</button>
-                </div>
-            </form>
-      </div>
-    </div>
-  )
-}
+  
 
 export default Almacen;
--- WEB/src/hooks/Almacen/useAlmacen.jsx
+++ WEB/src/hooks/Almacen/useAlmacen.jsx
@@ -1,35 +1,135 @@
 import { useState } from 'react'
+import Almacen from '../../features/Almacen/almacen'
 
-export default function useVentas(){
+export default function useAlmacen(){
     const [error, setError] = useState([])
-    
     const [loading, setLoading] = useState(false)
 
-    const ventas = async() =>{
-        setLoading(true)
-
+    const getAlmacen = async () => {
+        setLoading(true);
+        const token = localStorage.getItem("token");
         try {
-            const token = localStorage.getItem('token')
-            const response = await fetch("http://localhost:3000/ventas/",{
-                method: "GET",
+            const response = await fetch("http://localhost:3000/administrador/", {
                 headers: {
                     Authorization: "Bearer " + token,
                 }
-              
-            })
-            const data = await response.json()
-            if(!response.ok){
+            });
+            const data = await response.json();
+            if (!response.ok) {
+                if (data.errors) {
+                    setError(Array.isArray(data.errors) ? data.errors : [data.errors]);
+                    console.error(data.errors);
+                    return { success: false, errors: data.errors };
+                }
                 throw new Error(data.message || 'Error en la solicitud.');
             }
-            return {data}
+            return { data };
         } catch (error) {
-            setError(error.message)
-            return {success: false, message: error.message}
+            setError(error.message);
+            return { success: false, message: error.message };
+        } finally {
+            setLoading(false);
         }
-        finally {
-            setLoading(false)
+    };
+
+        const postAlmacen = async(almacen) =>{
+            setLoading(true)
+            const token = localStorage.getItem("token")
+            try {
+                const response = await fetch("http://localhost:3000/administrador/",{
+                    method: "POST",
+                    headers: {
+                        Authorization: "Bearer " + token,
+                        'content-type': 'application/json'
+                    },
+                    body: JSON.stringify(almacen)
+                })
+                const data = await response.json()
+                if (!response.ok) {
+                    if (data.errors) {
+                        setError(Array.isArray(data.errors) ? data.errors : [data.errors]);
+                        console.error(data.errors);
+                        return {success: false, errors: data.errors} 
+                    }
+                    throw new Error(data.message || 'Error en la solicitud.');
+                }
+                return {data}
+            } catch (error) {
+                setError(error.message)
+                return {success: false, message: error.message}
+                
+            }
+            finally {
+                setLoading(false)
+            }
         }
+
+        const putAlmacen = async(almacen) =>{
+            setLoading(true)
+            const token = localStorage.getItem("token")
+            try {
+                const response = await fetch(`http://localhost:3000/administrador/${Almacen.id}`,{
+                    method: "PUT",
+                    headers: {
+                        
+                        Authorization: "Bearer " + token,
+                        'content-type': 'application/json'
+                    },
+                    body: JSON.stringify(almacen)
+                })
+                const data = await response.json()
+                if (!response.ok) {
+                    if (data.errors) {
+                        setError(Array.isArray(data.errors) ? data.errors : [data.errors]);
+                        console.error(data.errors);
+                        return {success: false, errors: data.errors} 
+                    }
+                    throw new Error(data.message || 'Error en la solicitud.');
+                }
+                return {data}
+            } catch (error) {
+                setError(error.message)
+                return {success: false, message: error.message}
+            }
+            finally {
+                setLoading(false)
+            }
+        }
+
+        const deleteAlmacen = async(id) =>{
+            setLoading(true)  
+            const token = localStorage.getItem("token")
+            try {
+                const response = await fetch("http://localhost:3000/administrador/",{
+                    method: "DELETE",
+                    headers: {
+                        Authorization: "Bearer " + token,
+                        'content-type': 'application/json'
+                    },
+                    body: JSON.stringify({id})
+                })
+                const data = await response.json()
+                if (!response.ok) {
+                    if (data.errors) {
+                        setError(Array.isArray(data.errors) ? data.errors : [data.errors]);
+                        console.error(data.errors);
+                        return {success: false, errors: data.errors} 
+                    }
+                    throw new Error(data.message || 'Error en la solicitud.');
+                }
+                return {data}
+            } catch (error) {
+                setError(error.message)
+                return {success: false, message: error.message}
+            }
+            finally {
+                setLoading(false)
+            } 
+        }
+        
+        return {getAlmacen, postAlmacen, putAlmacen, deleteAlmacen, setError,error, loading}
+     
     }  
 
-    return {ventas, error, loading}
-}
+
+
