b7e8cb5c0b7a42d84ff86d04c5d384e1a470285e
Alan Carrizo <alancarrizo1017@gmail.com>
2024-11-23 23:15:00 -0300

    WEB: Cambios en feature Lobby con nuevo estilo de presentación del proyecto.

--- WEB/src/features/Lobby/lobby.jsx
+++ WEB/src/features/Lobby/lobby.jsx
@@ -4,7 +4,19 @@ function Lobby() {
 
   return (
     <div className='lobby-container'>
-      <h1 className='titulo'>Minimarket Bitward</h1>
+      <div className='pre-presentacion-container'>
+        <div className='presentacion-container'>
+          <h1 className='titulo'>MINIMARKET BITWARD</h1>
+          <p className='desc'>Proyecto universitario sobre aplicación de enfoque vendedor de un minimarket, con el objetivo de administrar productos para su venta con generación de ticket de la misma con todos sus detalles.</p>
+          <ul>
+            <li><p className='nombres'>Alarcón Mauro</p></li>
+            <li><p className='nombres'>Carrizo Alan</p></li>
+            <li><p className='nombres'>Cuello Joaquín</p></li>
+            <li><p className='nombres'>Francisco</p></li>
+          </ul>
+        </div>
+        <a href="/productos">Ir a los Productos...</a>
+      </div>
     </div>
   )
 }
